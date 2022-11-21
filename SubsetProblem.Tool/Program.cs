37c1fbc40459012f907f97271691a69396c60950
Rob Docherty <robert.g.docherty@gmail.com>
2022-11-21 22:45:19 +0000

    Break up main routine

--- SubsetProblem.Tool/Program.cs
+++ SubsetProblem.Tool/Program.cs
@@ -19,30 +19,13 @@ namespace SubsetProblem.Tool
             while (true)
             {
                 run++;
-
-                var seed = Guid.NewGuid().ToString();
+                var seed = GetSeed();
                 var candidate = new Candidate(seed);
                 var solutions = candidate.TargetSolutionSet.Count;
 
                 if (!SingleSolutionsOnly || (SingleSolutionsOnly && solutions == 1))
                 {
-
-                    WriteLine($"Run                 : {run} of {MaxRuns}");
-                    WriteLine($"Seed was            : {seed}");
-                    WriteLine($"Members in set      : {candidate.SourceNumberSet.Count}");
-                    WriteLine($"Set                 : {candidate.GetDisplayNumberSet()}");
-                    WriteLine($"TargetSum              : {candidate.TargetSum}");
-
-                    var literal = solutions == 0 ? "None" : solutions.ToString();
-
-                    WriteLine($"Number of solutions : {literal}");
-
-                    if (candidate.TargetSolutionSet.Count > 0)
-                    {
-                        WriteLine($"Solutions           : {candidate.GetSolveSet()}");
-                    }
-
-                    WriteLine();
+                    DisplayOutputSet(run, MaxRuns, seed, candidate, solutions);
                 }
 
                 if (run == MaxRuns)
@@ -53,6 +36,36 @@ namespace SubsetProblem.Tool
 
             sw.Stop();
 
+            DisplayTimingInformation(sw);
+        }
+
+        private static string GetSeed()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        private static void DisplayOutputSet(int run, int maxRuns, string seed, Candidate candidate, int solutions)
+        {
+            WriteLine($"Run                 : {run} of {maxRuns}");
+            WriteLine($"Seed was            : {seed}");
+            WriteLine($"Members in set      : {candidate.SourceNumberSet.Count}");
+            WriteLine($"Set                 : {candidate.GetDisplayNumberSet()}");
+            WriteLine($"TargetSum              : {candidate.TargetSum}");
+
+            var literal = solutions == 0 ? "None" : solutions.ToString();
+
+            WriteLine($"Number of solutions : {literal}");
+
+            if (candidate.TargetSolutionSet.Count > 0)
+            {
+                WriteLine($"Solutions           : {candidate.GetSolveSet()}");
+            }
+
+            WriteLine();
+        }
+
+        private static void DisplayTimingInformation(Stopwatch sw)
+        {
             WriteLine();
             WriteLine($"Ran in: {sw.ElapsedMilliseconds / 1000} s");
             WriteLine($"        {sw.ElapsedMilliseconds} ms");
